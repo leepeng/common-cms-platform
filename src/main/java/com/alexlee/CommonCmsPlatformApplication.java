83596a6ed40a64cdd0ab55e6b9d86acdfdd04adf
李鹏 <lp@zving.com>
2019-04-04 17:21:46 +0800

    feat: 项目初始化
    项目初始化

---
+++ src/main/java/com/alexlee/CommonCmsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.alexlee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonCmsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonCmsPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexlee/CommonCmsPlatformApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alexlee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommonCmsPlatformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
